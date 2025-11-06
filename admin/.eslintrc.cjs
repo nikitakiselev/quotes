4a4f21a69825bc71cdd7a7f3ae54baf7d7b917d5
Nikita Kiselev <kiselev2008@gmail.com>
2025-11-06 21:31:13 +0300

    ci: add GitHub Actions workflow with linters and tests
    - Add Go backend linting with golangci-lint
    - Add frontend and admin ESLint checks
    - Add backend tests with PostgreSQL service
    - Add Docker build verification
    - Remove deployment workflow

---
+++ admin/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
+}
+
