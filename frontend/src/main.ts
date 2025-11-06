2589115c40daab7dbc1cb65a2c1e90ac60bc5779
Nikita Kiselev <kiselev2008@gmail.com>
2025-11-06 21:34:52 +0300

    fix: remove Admin route from frontend router

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,11 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 import Home from './views/Home.vue'
-import Admin from './views/Admin.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/admin', name: 'Admin', component: Admin },
 ]
 
 const router = createRouter({
