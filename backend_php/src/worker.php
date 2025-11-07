ef596b6572de1a4f95976264041965729a84ef00
Nikita Kiselev <kiselev2008@gmail.com>
2025-11-08 01:47:50 +0300

    Add debug logging to worker initialization

--- backend_php/src/worker.php
+++ backend_php/src/worker.php
@@ -13,16 +13,23 @@ use App\Router;
 
 // Инициализация приложения
 try {
+    fwrite(STDERR, "Initializing application...\n");
     $config = new Config();
+    fwrite(STDERR, "Config loaded\n");
+    
     $database = new Database($config);
+    fwrite(STDERR, "Database connected\n");
+    
     $repository = new QuoteRepository($database->getPDO());
     $handlers = new Handlers($repository);
     $router = new Router($handlers);
+    fwrite(STDERR, "Router initialized\n");
     
     // Создание RoadRunner worker
     $worker = Worker::create();
     $psr17Factory = new Psr17Factory();
     $psr7Worker = new PSR7Worker($worker, $psr17Factory, $psr17Factory, $psr17Factory);
+    fwrite(STDERR, "Worker created, entering loop...\n");
     
     // Обработка запросов
     while ($request = $psr7Worker->waitRequest()) {
@@ -30,6 +37,7 @@ try {
             $response = $router->handle($request);
             $psr7Worker->respond($response);
         } catch (\Throwable $e) {
+            fwrite(STDERR, "Error handling request: " . $e->getMessage() . "\n");
             $psr7Worker->getWorker()->error((string)$e);
             $response = $psr17Factory->createResponse(500)
                 ->withHeader('Content-Type', 'application/json')
