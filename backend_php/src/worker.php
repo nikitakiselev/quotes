0989bf5c4613f58d97d895bd08af1d5903fe5396
Nikita Kiselev <kiselev2008@gmail.com>
2025-11-08 01:49:06 +0300

    Remove debug logging from PHP worker

--- backend_php/src/worker.php
+++ backend_php/src/worker.php
@@ -22,23 +22,16 @@ use App\Router;
 
 // Инициализация приложения
 try {
-    fwrite(STDERR, "Initializing application...\n");
     $config = new Config();
-    fwrite(STDERR, "Config loaded\n");
-    
     $database = new Database($config);
-    fwrite(STDERR, "Database connected\n");
-    
     $repository = new QuoteRepository($database->getPDO());
     $handlers = new Handlers($repository);
     $router = new Router($handlers);
-    fwrite(STDERR, "Router initialized\n");
     
     // Создание RoadRunner worker
     $worker = Worker::create();
     $psr17Factory = new Psr17Factory();
     $psr7Worker = new PSR7Worker($worker, $psr17Factory, $psr17Factory, $psr17Factory);
-    fwrite(STDERR, "Worker created, entering loop...\n");
     
     // Обработка запросов
     while ($request = $psr7Worker->waitRequest()) {
@@ -46,7 +39,6 @@ try {
             $response = $router->handle($request);
             $psr7Worker->respond($response);
         } catch (\Throwable $e) {
-            fwrite(STDERR, "Error handling request: " . $e->getMessage() . "\n");
             $psr7Worker->getWorker()->error((string)$e);
             $response = $psr17Factory->createResponse(500)
                 ->withHeader('Content-Type', 'application/json')
